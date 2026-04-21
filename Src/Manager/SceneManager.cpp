7ffe700638814dfafffe268f5d80c0fef4d822b6
ASOST\2416038 <yusei2589@icloud.com>
2026-04-21 15:34:27 +0900

    no message

--- Src/Manager/SceneManager.cpp
+++ Src/Manager/SceneManager.cpp
@@ -78,7 +78,7 @@ void SceneManager::Init3D(void)
 	ChangeLightTypeDir({ 0.3f, -0.7f, 0.8f });
 
 	// ÉtÉHÉOê›íË
-	SetFogEnable(true);
+	SetFogEnable(false);
 	SetFogColor(5, 5, 5);
 	SetFogStartEnd(10000.0f, 20000.0f);
 
